721f26d60d01719e57781ce9ace842f27179905b
Jojoe Sagoe <53071559+jsagoe1@users.noreply.github.com>
2019-09-26 20:02:19 -0500

    Update full_adder.v

--- VerilogBasics/Combinational/full_adder.v
+++ VerilogBasics/Combinational/full_adder.v
@@ -1,1 +1,26 @@
 
+// Code your design here
+module full_adder
+  (
+    sum,
+   	carry,
+   	a,
+   	b,
+   	cin
+  	);
+  
+  input a;
+  input b;
+  input cin;
+  output sum;
+  output carry;
+  
+  always @(a, b, cin)
+    begin
+      sum = a ^ b ^ c;
+      carry = (a&b) | (cin & (a^b))
+    end
+  
+endmodule
+
+
