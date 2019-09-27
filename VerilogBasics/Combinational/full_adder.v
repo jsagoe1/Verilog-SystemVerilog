658a3cdb49b3c9f1bd6b03a05a0d301010acb373
Jojoe Sagoe <53071559+jsagoe1@users.noreply.github.com>
2019-09-26 21:02:55 -0500

    Update full_adder.v

--- VerilogBasics/Combinational/full_adder.v
+++ VerilogBasics/Combinational/full_adder.v
@@ -16,3 +16,29 @@ module full_adder(sum, carry,	a, b,	cin);
 endmodule
 
 
+// full adder test bench
+//
+module test_fa ();
+  logic a, b, cin;
+  logic carry, sum;
+  
+  full_adder f1(a, b, cin, sum, carry);
+  
+  initial begin
+    a = 0;
+    b = 0;
+    cin = 0;
+    #100 $stop;
+  end
+  
+  always begin
+    #5; a = $random; b = $random;
+  end
+  
+  initial 
+    begin
+      $display ("    a/    b/   cin/   sum/   carry/");
+      $monitor ("%2d, %d/    %d/     %d/     %d/   %d/",$time, a, b, cin, sum, carry);
+    end
+  
+endmodule
