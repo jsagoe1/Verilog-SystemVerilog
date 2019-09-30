b8c0022af3a6307fd62b4dc0f9660ce085fc0586
Jojoe Sagoe <53071559+jsagoe1@users.noreply.github.com>
2019-09-30 18:01:58 -0500

    Create dram_sync.sv

---
+++ dram_sync.sv
@@ -0,0 +1,14 @@
+module dram_seq #(int m = 8, int n = 1024)
+  (output logic [m-1:0] 		data_out,
+   input logic [$clog2(n)-1:0] 	addr,		//2^k = n --> k = $clog2(n)
+   input logic 					clk,
+   input logic 					re);
+  
+  //memory array
+  logic [m-1:0] mem[n-1:0];
+  
+  always_ff @(posedge clk)
+    if (re)
+      data_out = mem[addr];
+  
+endmodule
