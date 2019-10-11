e990efe03494be33fcd9c265d3adc004214e5c1b
Jojoe Sagoe <53071559+jsagoe1@users.noreply.github.com>
2019-10-11 18:05:58 -0500

    Update mux_n.sv

--- mux_n.sv
+++ mux_n.sv
@@ -1,4 +1,4 @@
-module mux_n #(int w = 8, n = 4)
+module mux_n #(int w = 8, n = 4)    // w = data_width     n = number of inputs 
   (output logic [w-1:0] d_out,
    input uwire [w-1:0] d_in [0: n-1],
    input uwire [$clog2(n)-1:0] sel);
