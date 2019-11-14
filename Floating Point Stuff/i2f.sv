369b7c21f08fafd7edb6708a577667f5a8db8648
Jojoe Sagoe <jojoe.sagoe@yahoo.com>
2019-11-13 19:03:03 -0600

    Update i2f.sv

--- Floating Point Stuff/i2f.sv
+++ Floating Point Stuff/i2f.sv
@@ -1,12 +1,12 @@
-module	i2f	(									//convert	integer	to	float
-	output 	logic 			p_lost,				//precision lost
-	output	logic	[31:0]	a,					//float	
-	input	uwire	[31:0]	d);					//integer
+module	i2f	(										//convert	integer	to	float
+	output 	logic 			p_lost,					//precision lost
+	output	logic	[31:0]	a,						//float	
+	input	uwire	[31:0]	d);						//integer
 	
-	wire			sign	=	d[31];			//sign
-	wire	[31:0]	f5		=	sign? -d : d;		//absolute
+	wire			sign	=	d[31];				//sign
+	wire	[31:0]	f5		=	sign? -d : d;			//absolute
 	wire	[31:0]	f4,f3,f2,f1,f0;
-	wire	[4:0]	sa;						//shift	amount	(to	1.f)
+	wire	[4:0]	sa;							//shift	amount	(to	1.f)
 	
 	assign			sa[4]	=	̃|f5[31:16];					//16-bit 0
 	assign			f4	=	sa[4]? {f5[15:0], 16’b0} : f5;
@@ -22,5 +22,5 @@ module	i2f	(									//convert	integer	to	float
 	wire	[22:0]	fraction	=	f0[30:8];				//f0[31] = 1, hidden bit
 	wire	[7:0]	exponent	=	8’h9e - {3’h0, sa};		//0x9e	= 158 = 127 + 31
 	
-	assign			a	=	(d == 0)?	0 : {sign, exponent, fraction};
+	assign			a	=	(d == 0)? 0 : {sign, exponent, fraction};
 endmodule
